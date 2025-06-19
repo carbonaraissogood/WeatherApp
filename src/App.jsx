a3df36719e32ce286d12b2abff4ed1fa88062dcb
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-19 15:09:26 +0800

    feat: add App component with initial structure and imports

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import GETRequest from './GETRequest';
+import POSTRequest from './POSTRequest';
+import WeatherApp from './WeatherApp';
+
+function App() {
+  
+  return (
+    <div>
+      {/* <GETRequest></GETRequest> */}
+      {/* <POSTRequest></POSTRequest> */}
+
+      <WeatherApp></WeatherApp>
+    </div>
+  );
+};
+
+export default App
